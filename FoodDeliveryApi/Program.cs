d313565c821517dbbe0b7e1dd7fad194b62a0423
r0zymnuk <maksr240805@gmail.com>
2023-07-05 20:55:11 +0300

    Updated CORS Policy

--- FoodDeliveryApi/Program.cs
+++ FoodDeliveryApi/Program.cs
@@ -15,10 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "AllowLocalhost",
+    options.AddPolicy(name: "AllowAny",
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+                          policy.AllowAnyOrigin()
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
@@ -79,7 +79,7 @@ var app = builder.Build();
     app.UseSwaggerUI();
 // }
 
-app.UseCors("AllowLocalhost");
+app.UseCors("AllowAny");
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
